a9b984ac8973d5082ec62f68d06ecb7e32a9be4c
Aldy Sufriyanto <aldysufriyanto14@gmail.com>
2022-09-02 17:23:05 +0700

    change foto

--- database/seeders/FotoSeeder.php
+++ database/seeders/FotoSeeder.php
@@ -16,35 +16,35 @@ class FotoSeeder extends Seeder
     {
         Foto::create([
             'kamar_id' => 1,
-            'nama' => '0_No 1.Pemilik.jpg'
+            'nama' => '0_No 1.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 1,
-            'nama' => '1_No 1.Pemilik.jpg'
+            'nama' => '1_No 1.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 2,
-            'nama' => '0_No 2.Pemilik.jpg'
+            'nama' => '0_No 2.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 2,
-            'nama' => '1_No 2.Pemilik.jpg'
+            'nama' => '1_No 2.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 3,
-            'nama' => '0_No 3.Pemilik.jpg'
+            'nama' => '0_No 3.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 3,
-            'nama' => '1_No 3.Pemilik.jpg'
+            'nama' => '1_No 3.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 4,
-            'nama' => '0_No 4.Pemilik.jpg'
+            'nama' => '0_No 4.Pemilik.jpeg'
         ]);
         Foto::create([
             'kamar_id' => 4,
-            'nama' => '1_No 4.Pemilik.jpg'
+            'nama' => '1_No 4.Pemilik.jpeg'
         ]);
     }
 }
--- database/seeders/KamarSeeder.php
+++ database/seeders/KamarSeeder.php
@@ -19,34 +19,34 @@ class KamarSeeder extends Seeder
             'ukuran'    => '20 X 30 meter',
             'harga'     => '2,000,000',
             'deskripsi' => 'Kamar Paling dekat dengan pintu gerbang, menghadap ke arah timur',
-            'cover' => 'No 1.Pemilik.jpg',
+            'cover' => 'No 1.Pemilik.jpeg',
             'status' => 0,
             'tampil'    => 1
         ]);
         Kamar::create([
-            'pintu_id' => 1,
+            'pintu_id' => 2,
             'ukuran'    => '20 X 30 meter',
             'harga'     => '2,000,000',
             'deskripsi' => 'Kamar Paling dekat dengan pintu gerbang, menghadap ke arah timur',
-            'cover' => 'No 2.Pemilik.jpg',
+            'cover' => 'No 2.Pemilik.jpeg',
             'status' => 0,
             'tampil'    => 1
         ]);
         Kamar::create([
-            'pintu_id' => 1,
+            'pintu_id' => 3,
             'ukuran'    => '20 X 30 meter',
             'harga'     => '2,000,000',
             'deskripsi' => 'kamar berada di lantai 1  dengan jendela yang menghadap ke arah koridor.',
-            'cover' => 'No 3.Pemilik.jpg',
+            'cover' => 'No 3.Pemilik.jpeg',
             'status' => 0,
             'tampil'    => 1
         ]);
         Kamar::create([
-            'pintu_id' => 1,
+            'pintu_id' => 4,
             'ukuran'    => '20 X 30 meter',
             'harga'     => '2,000,000',
             'deskripsi' => 'kamar berada di lantai 2  dengan jendela yang menghadap ke arah koridor.',
-            'cover' => 'No 4.Pemilik.jpg',
+            'cover' => 'No 4.Pemilik.jpeg',
             'status' => 0,
             'tampil'    => 1
         ]);
